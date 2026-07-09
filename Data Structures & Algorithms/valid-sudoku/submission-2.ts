6db6a54c276d5837bca6cc6e05126dc0dff26270
Roman Yaresko <87436640+RomanYaresko@users.noreply.github.com>
2026-07-09 23:01:07 +0200

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.ts
@@ -0,0 +1,79 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board: string[][]): boolean {
+        const isValidRow = (rowIndex: number): boolean => {
+            const exists: Map<string, boolean> = new Map();
+            for (const rowItem of board[rowIndex]) {
+                if (rowItem == '.') {
+                    continue
+                }
+
+                if (exists.get(rowItem) == true) {
+                    return false
+                } else {
+                    exists.set(rowItem, true)
+                }
+            }
+
+            return true
+        }
+
+        const isValidColumn = (columnIndex: number): boolean => {
+            const exists: Map<string, boolean> = new Map();
+            for (let i = 0; i < board.length; i++) {
+                if (board[i][columnIndex] == '.') {
+                    continue
+                }
+
+                if (exists.get(board[i][columnIndex]) == true) {
+                    return false
+                } else {
+                    exists.set(board[i][columnIndex], true)
+                }
+            }
+
+            return true
+        }
+
+        const isValidSubBox = (subBoxRowIndex: number, subBoxColumnIndex: number): boolean => {
+            const exists: Map<string, boolean> = new Map();
+            for (let i = subBoxRowIndex; i < subBoxRowIndex + 3; i++) {
+                for (let k = subBoxColumnIndex; k < subBoxColumnIndex + 3; k++) {
+                    if (board[i][k] == '.') {
+                        continue
+                    }
+
+                    if (exists.get(board[i][k]) == true) {
+                        return false
+                    } else {
+                        exists.set(board[i][k], true)
+                    }
+                }
+            }
+
+            return true
+        }
+
+        for (let i = 0; i < board.length; i++) {
+            if (!isValidRow(i)) {
+                return false
+            }
+            for (let k = 0; k < board[0].length; k++) {
+                if (!isValidColumn(i)) {
+                    return false
+                }
+
+                if (i % 3 == 0 && k % 3 == 0) {
+                    if (!isValidSubBox(i, k)) {
+                        return false
+                    }
+                }
+            }
+        }
+
+        return true
+    }
+}
