79214bd64afb653261ee948262e5242f674e2bdb
Roman Yaresko <87436640+RomanYaresko@users.noreply.github.com>
2026-07-14 00:57:48 +0200

    Add: trapping-rain-water - submission-8

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-8.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height: number[]): number {
+        let totalArea: number = 0;
+
+        let leftIndex: number = 0;
+        let rightIndex: number = height.length - 1;
+
+        let maxLeft: number = height[leftIndex];
+        let maxRight: number = height[rightIndex];
+
+        while (leftIndex < rightIndex) {
+            if (height[leftIndex] <= height[rightIndex]) {
+                leftIndex++;
+                const area = maxLeft - height[leftIndex];
+                if (area > 0) {
+                    totalArea += area;
+                }
+            } else {
+                rightIndex--;
+                const area = maxRight - height[rightIndex];
+                if (area > 0) {
+                    totalArea += area;
+                }
+            }
+
+            maxLeft = Math.max(maxLeft, height[leftIndex]);
+            maxRight = Math.max(maxRight, height[rightIndex]);
+        }
+
+        return totalArea;
+    }
+}
