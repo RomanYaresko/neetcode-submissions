d0ae43143c4083908c34f22a9bac7ad7b0e9592a
Roman Yaresko <87436640+RomanYaresko@users.noreply.github.com>
2026-07-17 23:35:49 +0200

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.ts
@@ -0,0 +1,32 @@
+class MinStack {
+    stack: number[];
+    minStack: number[];
+
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val: number): void {
+        this.stack.push(val);
+
+        if (this.minStack.length === 0) {
+            this.minStack.push(val);
+        } else {
+            this.minStack.push(Math.min(val, this.getMin()))
+        }
+    }
+
+    pop(): void {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
