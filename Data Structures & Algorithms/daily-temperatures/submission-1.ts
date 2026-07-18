f2937a69137660aa9ba7c0d08d57dc2f4308793c
Roman Yaresko <87436640+RomanYaresko@users.noreply.github.com>
2026-07-19 01:27:45 +0200

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+        let monotonicStack: number[] = [];
+
+        let result: number[] = [];
+        for (let i = temperatures.length - 1; i >= 0; i--) {
+            result.push(0);
+
+            while (monotonicStack.length) {
+                const head = monotonicStack[monotonicStack.length - 1];
+                if (temperatures[head] <= temperatures[i]) {
+                    monotonicStack.pop();
+                } else {
+                    result[result.length - 1] = head - i;
+                    break;
+                }
+            }
+
+            monotonicStack.push(i);
+        }
+
+        result.reverse()
+        return result;
+    }
+}
